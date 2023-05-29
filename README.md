a86d7bc63150567f3f21d5395014ed1dc3b831d3
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2023-05-29 14:00:08 +0100

    feat: update student management system

--- README.md
+++ README.md
@@ -1,129 +1,34 @@
-![Logo of the project](https://raw.githubusercontent.com/jehna/readme-best-practices/master/sample-logo.png)
+# Student Records Management System
 
-# Module 1 Project
-
-> Additional information or tagline
-
-A stdent management system that allows a user to add, list, update and delete student records.
-
-## Installing / Getting started
-
-User first needs to sign in via a log-in system on the welcome page
-After providing required authentication details. User gets to perform any of the aforementioned operations.
-
-### Initial Configuration
-
-Some projects require initial configuration (e.g. access tokens or keys, `npm i`).
-This is the section where you would document those requirements.
-
-## Developing
-
-Here's a brief intro about what a developer must do in order to start developing
-the project further:
-
-```shell
-git clone https://github.com/your/awesome-project.git
-cd awesome-project/
-packagemanager install
-```
-
-And state what happens step-by-step.
-
-### Building
-
-If your project needs some additional steps for the developer to build the
-project after some code changes, state them here:
-
-```shell
-./configure
-make
-make install
-```
-
-Here again you should state what actually happens when the code above gets
-executed.
-
-### Deploying / Publishing
-
-In case there's some step you have to take that publishes this project to a
-server, this is the right time to state it.
-
-```shell
-packagemanager deploy awesome-project -s server.com -u username -p password
-```
-
-And again you'd need to tell what the previous code actually does.
+The Student Records Management System is a simple web application built using PHP and JSON file storage. It allows you to manage student records, including adding new students, updating their information, and deleting records.
 
 ## Features
 
-What's all the bells and whistles this project can perform?
+- Add new students with their name, class, registration number -assigned internally using the php in-built time() function-, and grade.
+- View a list of all students with their details.
+- Delete students from the records.
+- Update student information.
 
-- What's the main functionality
-- You can also do another thing
-- If you get really randy, you can even do this
+## Installation
 
-## Configuration
+1. Clone the repository or download the source code.
+2. Make sure you have PHP installed on your system.
+3. Start the PHP built-in server: `php -S localhost:8000`.
+4. Open the application in your browser: [http://localhost:8000/index.php](http://localhost:8000/index.php).
+5. Make sure you have an internet connection as the styling and responsiveness of the page depends on a CDN: Content Delivery Network.
 
-Here you should write what are all of the configurations a user can enter when
-using the project.
+## Usage
 
-#### Argument 1
+- Click the "New Student" button to add a new student to the records. Fill in the required information in the modal and click "Save."
+- The list of students will be displayed in a table. You can delete a student by clicking the "Delete" button next to their record.
+- To update a student's information, click the "Update" button in the "Action" column. You will be redirected to the update form where you can make changes and save them.
 
-Type: `String`  
-Default: `'default value'`
+## Technologies
 
-State what an argument does and how you can use it. If needed, you can provide
-an example below.
-
-Example:
-
-```bash
-awesome-project "Some other value"  # Prints "You're nailing this readme!"
-```
-
-#### Argument 2
-
-Type: `Number|Boolean`  
-Default: 100
-
-Copy-paste as many of these as you need.
+- PHP: Server-side scripting language used for handling data and rendering views.
+- JSON: File-based storage used to store the student records.
+- Bootstrap: Front-end framework for responsive and modern user interface design.
 
 ## Contributing
 
-When you publish something open source, one of the greatest motivations is that
-anyone can just jump in and start contributing to your project.
-
-These paragraphs are meant to welcome those kind souls to feel that they are
-needed. You should state something like:
-
-"If you'd like to contribute, please fork the repository and use a feature
-branch. Pull requests are warmly welcome."
-
-If there's anything else the developer needs to know (e.g. the code style
-guide), you should link it here. If there's a lot of things to take into
-consideration, it is common to separate this section to its own file called
-`CONTRIBUTING.md` (or similar). If so, you should say that it exists here.
-
-## Links
-
-Even though this information can be found inside the project on machine-readable
-format like in a .json file, it's good to include a summary of most useful
-links to humans using your project. You can include links like:
-
-- Project homepage: https://your.github.com/awesome-project/
-- Repository: https://github.com/your/awesome-project/
-- Issue tracker: https://github.com/your/awesome-project/issues
-  - In case of sensitive bugs like security vulnerabilities, please contact
-    my@email.com directly instead of using issue tracker. We value your effort
-    to improve the security and privacy of this project!
-- Related projects:
-  - Your other project: https://github.com/your/other-project/
-  - Someone else's project: https://github.com/someones/awesome-project/
-
-## Licensing
-
-One really important part: Give your project a proper license. Here you should
-state what the license is and how to find the text version of the license.
-Something like:
-
-"The code in this project is licensed under MIT license."
+Contributions are welcome! If you find any issues or have suggestions for improvements, please open an issue or submit a pull request.
--- index.php
+++ index.php
@@ -1,67 +1,178 @@
+<?php
+
+$filePath = __DIR__ . DIRECTORY_SEPARATOR . 'database.json';
+
+// Read the database file
+$data = file_get_contents($filePath);
+$members = json_decode($data, true);
+
+// Check if the form is submitted
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Retrieve the form data and clean the values using the trim and strip_tags functions
+    $name = ucwords(trim(strip_tags($_POST['name'])));
+    $class = strtolower(trim(strip_tags($_POST['class'])));
+    $grade = trim(strip_tags($_POST['grade']));
+
+    // Generate the registration number using the time() function
+    $regNo = time(); // The registration number will serve as the student's primary key
+
+    // Create a new member record
+    $newMember = array(
+        'name' => $name,
+        'class' => $class,
+        'regNo' => $regNo,
+        'grade' => $grade
+    );
+
+    // Add the new member to the array
+    $members[] = $newMember;
+
+    // Save the updated records back to the file
+    $updatedRecord = json_encode($members, JSON_PRETTY_PRINT);
+    file_put_contents($filePath, $updatedRecord);
+
+    // Redirect back to index.php
+    header('Location: index.php');
+    exit;
+}
+
+// Check if the delete action is requested
+if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['action']) && $_GET['action'] === 'delete' && isset($_GET['regNo'])) {
+    $regNo = $_GET['regNo'];
+
+    // Search for the member with the matching registration number
+    foreach ($members as $key => $member) {
+        if ($member['regNo'] == $regNo) {
+            // Remove the existing member record
+            unset($members[$key]);
+            break;
+        }
+    }
+
+    // Save the updated records back to the file
+    $updatedRecord = json_encode($members, JSON_PRETTY_PRINT);
+    file_put_contents($filePath, $updatedRecord);
+
+    // Redirect back to index.php
+    header('Location: index.php');
+    exit;
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css"
-      rel="stylesheet"
-    />
-    <title>Student Management System</title>
-  </head>
-  <body>
-    <div class="container my-5">
-      <form action="main.php" method="POST">
-        <div class="mb-3">
-          <label for="name" class="form-label"><strong>Name:</strong></label>
-          <input
-            type="text"
-            class="form-control"
-            id="name"
-            name="name"
-            placeholder="Enter Student's First and Last Names"
-          />
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="styles.css">
+    <title>Student Records</title>
+</head>
+
+<body class="my-5 mb-5">
+
+    <div class="container">
+        <div class="float-end mt-2">
+            <!-- Button trigger modal -->
+            <button type="button" class="btn btn-primary mb-3" data-bs-toggle="modal" data-bs-target="#newStudentModal">
+                New Student
+            </button>
         </div>
-        <div class="mb-3">
-          <label for="registration" class="form-label"
-            ><strong>Registration No.:</strong></label
-          >
-          <input
-            type="text"
-            class="form-control"
-            id="registration"
-            name="regNo"
-            placeholder="Reg. No."
-          />
-        </div>
-        <div>
-          <label for="grade" class="form-label"><strong>Grade:</strong></label>
-          <input
-            type="text"
-            class="form-control"
-            id="grade"
-            name="grade"
-            placeholder="Enter Grade"
-          />
-        </div>
-        <div id="gradehelpinfo" class="form-text mb-3">
-          Grade must be positive integers between 0-10; it cannot be negative
-          numbers or contain characters, or emoji.
-        </div>
-        <div class="mb-3">
-          <label for="classroom" class="form-label"
-            ><strong>Class:</strong></label
-          >
-          <select class="form-select" id="classroom" name="class">
-            <option selected disabled>Click to Select</option>
-            <option value="frontend">Frontend</option>
-            <option value="backend">Backend</option>
-          </select>
-        </div>
-        <button type="submit" class="btn btn-primary" name="submit">
-          Submit
-        </button>
-      </form>
     </div>
-  </body>
+
+    <section class="wrapper-main">
+        <!-- List the users -->
+        <div class="container table-wrapper">
+            <table class="table table-striped table-bordered mt-4">
+                <thead class="thead-dark">
+                    <tr>
+                        <th>S/n</th>
+                        <th>Name</th>
+                        <th>Class</th>
+                        <th>Reg. No.</th>
+                        <th>Grade</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    if (!empty($members)) {
+                        $count = 0;
+                        foreach ($members as $row) {
+                            $count++;
+                    ?>
+                            <tr>
+                                <td><?php echo $count; ?></td>
+                                <td><?php echo $row["name"]; ?></td>
+                                <td><?php echo $row["class"]; ?></td>
+                                <td><?php echo $row["regNo"]; ?></td>
+                                <td><?php echo $row["grade"]; ?></td>
+                                <td>
+                                    <a href="update.php?regNo=<?php echo $row["regNo"]; ?>" class="btn btn-primary btn-sm">Update</a>
+                                    <a href="index.php?action=delete&regNo=<?php echo $row["regNo"]; ?>" class="btn btn-danger btn-sm">Delete</a>
+                                </td>
+                            </tr>
+                        <?php
+                        }
+                    } else {
+                        ?>
+                        <tr>
+                            <td colspan="6">No records found.</td>
+                        </tr>
+                    <?php
+                    }
+                    ?>
+                </tbody>
+            </table>
+        </div>
+    </section>
+
+    <!-- New Student Modal -->
+    <div class="modal fade" id="newStudentModal" tabindex="-1" aria-labelledby="newStudentModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="newStudentModalLabel"><strong>New Student Record</strong> </h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form method="post" action="index.php">
+                        <!-- Name Field -->
+                        <div>
+                            <label for="name" class="form-label"><strong>Name:</strong></label>
+                            <input type="text" class="form-control" id="name" name="name" autocomplete="off">
+                        </div>
+                        <div class="mb-3">
+                            <p><em>First and Last Names Only</em> </p>
+                        </div>
+                        <!-- Class Field -->
+                        <div>
+                            <label for="class" class="form-label"><strong>Class:</strong></label>
+                            <input type="text" class="form-control" id="class" name="class" autocomplete="off">
+                        </div>
+                        <div class="mb-3">
+                            <p><em>Stem | Commerce | Art</em> </p>
+                        </div>
+                        <!-- Grade Field -->
+                        <div class="mb-3">
+                            <label for="grade" class="form-label">
+                                <strong>Grade:</strong>
+                            </label>
+                            <input type="number" class="form-control" id="grade" name="grade" autocomplete="off">
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="submit" class="btn btn-primary">Save</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
 </html>
