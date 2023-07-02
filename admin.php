72f74ee279461641b96f856f40ef121cf6e19f62
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2023-07-02 18:26:58 +0100

    feat: setting-up interface

--- admin.php
+++ admin.php
@@ -16,6 +16,7 @@ $operation = new DbTableOps($conn);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@1/css/pico.fluid.classless.min.css">
+    <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@1/css/pico.min.css" /> -->
     <link rel="stylesheet" href="style.css">
     <title>Jagaad Academy</title>
 </head>
